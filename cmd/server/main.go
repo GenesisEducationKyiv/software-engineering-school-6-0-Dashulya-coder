42a801474e2370b6471ba4000b211efb1e426884
boss <a@no.pics>
2026-04-12 21:47:45 +0300

    feat: implement repository package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
 	"github.com/Dashulya-coder/CaseTaskNotifier/internal/app"
 	"github.com/Dashulya-coder/CaseTaskNotifier/internal/config"
-	"github.com/Dashulya-coder/CaseTaskNotifier/internal/repository"
 )
 
 func main() {
@@ -24,18 +22,7 @@ func main() {
 	defer db.Close()
 
 	log.Println("database connected successfully")
-
-	subRepo := repository.NewSubscriptionRepository(db)
-
-	subs, err := subRepo.GetConfirmedActiveByRepo(context.Background(), 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, s := range subs {
-		log.Printf("confirmed active subscription for repo: %+v\n", s)
-	}
-
+	
 	log.Println("server started on :" + cfg.Port)
 	if err := http.ListenAndServe(":"+cfg.Port, http.NewServeMux()); err != nil {
 		log.Fatal(err)
